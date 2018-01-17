dd55e7cd6e6869b7ae05c68b187ebbb568105dab
TuranHuseynov <turaneh@code.edu.az>
2018-01-17 18:09:09 +0400

    ...

--- WindowsFormsApp19/Form1.Designer.cs
+++ WindowsFormsApp19/Form1.Designer.cs
@@ -29,11 +29,82 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pontos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(32, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pontos
+            // 
+            this.pontos.AutoSize = true;
+            this.pontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pontos.ForeColor = System.Drawing.Color.CadetBlue;
+            this.pontos.Location = new System.Drawing.Point(497, 57);
+            this.pontos.Name = "pontos";
+            this.pontos.Size = new System.Drawing.Size(100, 29);
+            this.pontos.TabIndex = 2;
+            this.pontos.Text = "Pontos :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label1.Location = new System.Drawing.Point(571, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 353);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pontos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label pontos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WindowsFormsApp19/Form1.cs
+++ WindowsFormsApp19/Form1.cs
@@ -3,18 +3,180 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp19
 {
     public partial class Form1 : Form
     {
+        byte Dir = 1;
+        byte score = 0;
+        int ContPcb = 3;
+
+        PictureBox[] pic = new PictureBox[100];
+        PictureBox NovPcb = new PictureBox();
+
+
+
         public Form1()
         {
             InitializeComponent();
+            IniciarLayout();
+
+        }
+
+        private void IniciarLayout()
+        {
+            for(byte cont = 1; cont <= 3; cont++)
+            {
+                pic[cont] = new PictureBox();
+                pic[cont].Width = 20;
+                pic[cont].Height = 20;
+                pic[cont].Left = cont* 20;
+                pic[cont].Top = 20;
+                pic[cont].BackColor = Color.Black;
+
+                this.panel1.Controls.Add(pic[cont]);
+                
+            }
+
+            NovPcb.Width = 20;
+            NovPcb.Height = 20;
+            NovPcb.BackColor = Color.CornflowerBlue;
+
+            RndNovoLoc();
+
+            this.panel1.Controls.Add(NovPcb);
+            timer1.Start();
+
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if(Dir != 2)
+                    {
+                        Dir = 1;
+                    }
+                    break;
+
+                case Keys.Left:
+                    if(Dir != 1)
+                    {
+                        Dir = 2;
+                    }
+                    break;
+
+                case Keys.Down:
+                    if(Dir != 4)
+                    {
+                        Dir = 3;
+                    }
+                    break;
+
+                case Keys.Up:
+                    if (Dir != 3)
+                    {
+                        Dir = 4;
+                    }
+                    break;
+            }
+        }
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Movimento();
+            
+        }
+        private void Movimento()
+        {
+            Point[] pt = new Point[2];
+            pt[0] = pic[1].Location;
+
+            switch (Dir)
+            {
+                case 1:
+                    pic[1].Left += 20;
+                    break;
+                case 2:
+                    pic[1].Left -= 20;
+                    break;
+                case 3:
+                    pic[1].Top += 20;
+                    break;
+                case 4:
+                    pic[1].Top -= 20;
+                    break;
+            }
+
+            for(int cont = 2; cont <= ContPcb; cont++)
+            {
+                pt[1] = pic[cont].Location;
+                pic[cont].Location = pt[0];
+                pt[0] = pt[1];
+
+            }
+
+            Condicoes();
+        }
+
+        private void Condicoes()
+        {
+            if (pic[1].Location == NovPcb.Location)
+            {
+                score += 1;
+                label1.Text = score.ToString();
+                RndNovoLoc();
+                 AddPic();
+
+            }
+            else if (pic[1].Location.X < 0 || pic[1].Location.X > 400 || pic[1].Location.Y < 0 || pic[1].Location.Y > 300 )
+            {
+                timer1.Stop();
+                button1.Enabled = true;
+                MessageBox.Show("Oyun Bitti");
+
+            }
+
+        }
+
+        private void RndNovoLoc()
+        {
+            Random random = new Random();
+            Point RndPt = new Point(20 * random.Next(20), 20 * random.Next(15));
+            NovPcb.Location = RndPt;
+        }
+
+        private void AddPic()
+        {
+            ContPcb += 1;
+            
+            pic[ContPcb] = new PictureBox();
+            pic[ContPcb].Width = 20;
+            pic[ContPcb].Height = 20;
+            pic[ContPcb].BackColor = Color.Black;
+            pic[ContPcb].Left = -20;
+
+            this.panel1.Controls.Add(pic[ContPcb]);
+        }
+
+         private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            Dir = 1;
+            ContPcb = 3;
+            IniciarLayout();
+            button1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
